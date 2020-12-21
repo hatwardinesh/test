83acc4097b1a01f199eb15ba45b08fcc413831de
Hatwar Dinesh <hatwar.dinesh@gmail.com>
2020-12-22 01:52:34 +0530

    Added other practice file

---
+++ Assignemnt.py
@@ -0,0 +1,15 @@
+a= 1
+print(a)
+b= 3.14
+print(b)
+has_0_valid = "Looks like"
+print(has_0_valid)
+
+print(type(b))
+
+#multiple assignment
+a, b, c = 1, 1.2, "string"
+print(a,b,c)
+
+a=b=c=1
+print(a,b,c)
---
+++ List.py
@@ -0,0 +1,10 @@
+a= [1,2,[3,2,4], 2,4]
+print(a[2])
+print(a[2][2])
+print(a[1] * a[3])
+
+tple= (1,1,2,3)
+print(tple)
+#tple[1]= 3
+a= '123.34'
+b= int(a)
